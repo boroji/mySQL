a280ba0a0c8b96395e26e387288144d22473b86d
Hossein <hosseinboroji@gmail.com>
2020-05-23 23:19:02 -0400

    creating markup and styling for the login database

--- index.php
+++ index.php
@@ -1,14 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="img/png" href="img/myHeart.png">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/style.css">
-    <title>My SQL Login</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php include "includes/header.php" ?>
+
+
+<div class="wrapper">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-6">
+                <form action="index.php" method="post">
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input class="form-control" type="text" name="username" id="username">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input class="form-control" type="password" name="password" id="password">
+                    </div>
+                    <div class="form-group d-flex flex-column flex-lg-row">
+                        <input class="btn btn-primary mb-3 mr-lg-3 mr-lg-0" type="submit" name="create" value="Create">
+                        <input class="btn btn-secondary mb-3 mr-lg-3 mr-lg-0" type="submit" name="read" value="Read">
+                        <input class="btn btn-success mb-3 mr-lg-3 mr-lg-0" type="submit" name="update" value="Update">
+                        <input class="btn btn-danger mb-3 mr-lg-3 mr-lg-0" type="submit" name="delete" value="Delete">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include "includes/footer.php" ?>
